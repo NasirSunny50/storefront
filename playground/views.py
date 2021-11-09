cac16a69689cbbd62f3be7e029f91d106891eef0
NasirSunny50 <nasir.uddin08@northsouth.edu>
2021-11-09 21:38:55 +0600

    First Commit

---
+++ playground/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+#Request -> Response 
+# Request Handler
+
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request, 'hello.html', {'name': 'Sony'})
